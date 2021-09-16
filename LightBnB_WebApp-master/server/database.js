15a306220818f5a6fab0c119db6d524795738829
Lewis Lee <lewisforbusiness@gmail.com>
2021-09-16 16:46:48 -0400

    Modified getAllProperties so that it takes the options argument

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -1,3 +1,4 @@
+const { query } = require('express');
 const { Pool } = require('pg');
 
 const pool = new Pool({
@@ -12,17 +13,64 @@ const properties = require('./json/properties.json');
 const users = require('./json/users.json');
 
 
-const getAllProperties = (options, limit = 10) => {
-  return pool
-  .query(
-    `
-    SELECT * 
-    FROM properties
-    LIMIT $1
-    `, [limit])
-    .then((result) => result.rows)
-    .catch((err) => console.log('ERR!!!!!:',err.message));
+const getAllProperties = function (options, limit = 10) {
+  console.log('options: ', options)
+  // 1
+  const queryParams = [];
+  // 2
+  let queryString = `
+  SELECT properties.*, avg(property_reviews.rating) as average_rating
+  FROM properties
+  JOIN property_reviews ON properties.id = property_id
+  `;
+
+  console.log('queryString Before : ', queryString);
+
+  // 3
+  if (options.city) {
+    queryParams.push(`%${options.city}%`);
+    queryString += `WHERE city LIKE $${queryParams.length} `;
   }
+ 
+  if (options.owner_id) {
+    queryParams.push(`${options.owner_id}`)
+    queryString += `AND owner_id = $${queryParams.length} `;
+  }
+
+  if (options.minimum_price_per_night && options.maximum_price_per_night) {
+    console.log('min nights hit')
+    queryParams.push(`${options.minimum_price_per_night}`);
+    queryString += `AND cost_per_night >= $${queryParams.length} `;
+    queryParams.push(`${options.maximum_price_per_night}`);
+    queryString += `AND cost_per_night <= $${queryParams.length} `;
+  }
+
+  // 4
+  queryString += `
+  GROUP BY properties.id `
+  
+  if (options.minimum_rating) {
+    queryParams.push(`${options.minimum_rating}`);
+    queryString += `HAVING avg(property_reviews.rating) >= $${queryParams.length} `;
+  }
+
+  queryParams.push(limit);
+  queryString += `
+  ORDER BY cost_per_night
+  LIMIT $${queryParams.length};
+  `;
+
+  // 5
+  
+  console.log('queryParams :', queryParams, 'queryString :', queryString);
+
+  // 6
+  return pool
+  .query(queryString, queryParams)
+  .then((res) => {
+    return res.rows
+  });
+};
 
 exports.getAllProperties = getAllProperties;
 
@@ -118,7 +166,7 @@ const getAllReservations = function(guest_id, limit = 10) {
     LIMIT $2;
   `, [guest_id, limit])
   // When I put LIMIT $2, it does not work
-  .then(result => console.log(result.rows));
+  .then(result => result.rows);
 }
 exports.getAllReservations = getAllReservations;
 
@@ -146,9 +194,22 @@ exports.getAllReservations = getAllReservations;
  * @return {Promise<{}>} A promise to the property.
  */
 const addProperty = function(property) {
-  const propertyId = Object.keys(properties).length + 1;
-  property.id = propertyId;
-  properties[propertyId] = property;
-  return Promise.resolve(property);
+  console.log(property)
+  const { owner_id, title, description, thumbnail_photo_url, cover_photo_url, cost_per_night, parking_spaces, 
+    number_of_bathrooms, number_of_bedrooms, country, street, city, province, post_code} = property; 
+  const values = [owner_id, title, description, thumbnail_photo_url, cover_photo_url, cost_per_night, parking_spaces, 
+    number_of_bathrooms, number_of_bedrooms, country, street, city, province, post_code, true];
+  return pool. 
+    query(`
+      INSERT INTO properties (owner_id, title, description, thumbnail_photo_url, cover_photo_url, cost_per_night, parking_spaces, 
+        number_of_bathrooms, number_of_bedrooms, country, street, city, province, post_code, active)
+        VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15)
+        RETURNING *;
+    `, values)
+    .then(result => result.rows)
+  // const propertyId = Object.keys(properties).length + 1;
+  // property.id = propertyId;
+  // properties[propertyId] = property;
+  // return Promise.resolve(property);
 }
 exports.addProperty = addProperty;
